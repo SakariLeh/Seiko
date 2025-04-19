bf0d571f8e598a2d19c3dad28c80e091ab834793
SakariLeh <100150788+SakariLeh@users.noreply.github.com>
2025-04-19 21:54:12 +0500

    Create run.py
    Пересоздал

---
+++ run.py
@@ -0,0 +1,12 @@
+from app import create_app
+from app.env import env_config
+
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(
+        debug=env_config.get('IS_DEBUG', True), 
+        port=env_config.get("FLASK_RUN_PORT", 5000)
+    )
